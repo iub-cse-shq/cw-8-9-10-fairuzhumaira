9f3ba479386d9187756d6f3a04e62e979844137c
fairuzhumaira <hhfairuz@gmail.com>
2021-01-10 00:50:52 +0600

    cw

--- classwork/server.js
+++ classwork/server.js
@@ -99,5 +99,4 @@ server.listen(process.env.PORT || 3000,
 process.env.IP || 'localhost', function(){
   				console.log('Server running');
 })
-module.exports = {app, server}
 module.exports = {app, server, mongoose}
